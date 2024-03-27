38d3cc85e9d43572df040bbc01c40a3ef7a63d78
naveenorugati1 <naveenorugati1@gmail.com>
2024-03-27 06:04:04 +0530

    Handler mapping Expample

---
+++ handlermappingapp/src/main/java/com/hma/web/AboutUsController.java
@@ -0,0 +1,17 @@
+package com.hma.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class AboutUsController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("about-us");
+		return mav;
+	}
+}
---
+++ handlermappingapp/src/main/webapp/WEB-INF/views/about-us.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>About US</h1>
+	<p style="font: 15px;font-family: sans-serif;">Welcome to Infra About US</p>
+</body>
+</html>
